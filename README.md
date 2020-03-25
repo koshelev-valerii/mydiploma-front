7fe5f4dc5c9d14ee9697401a7888775d084adfcc
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-03-25 19:51:18 +0100

    Init app

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Hello World!
+# MyDiploma-Front author Valerii Koshelev
+-------------------------------
+
+### Version 0.1.0
+
+## Description
+-------------------------------
+
+### The package is a Yandex Praktikum project that contains the implementation of the frontend part of the my diploma
+
+## Where you can find me?
+-------------------------------
+
+### [Link to the project](https://koshelev-valerii.github.io/MyDiploma-Front/)
+
+
+`https://mydiploma.website/` (domain for front part)
+
+`https://www.mydiploma.website/` (domain for front part)
+
+## Getting Started
+
+### Download and run:
+
+`git clone https://github.com/koshelev-valerii/mydiploma-front.git`
+
+
+`npm install`
+
+`npm run start` (for build)
+
+`npm run dev` (for start with hot reload)
+
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssnano = require('cssnano');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    articles: './src/articles/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          (isDev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } }),
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './images/[name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              svgo: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.85, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 85,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './vendor/fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/articles/index.html',
+      filename: './articles/index.html',
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
+  ],
+};
