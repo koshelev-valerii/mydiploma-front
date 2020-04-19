6ae2a74de9566b18e9baa3d3556cc6bea947108d
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-04-19 23:02:04 +0200

    Fixed bugs

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,18 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/env',
-      {
-        targets: {
-          android: '67',
-          ios: '12',
-          edge: '15',
-          firefox: '60',
-          chrome: '64',
-          safari: '11.1',
-        },
-        useBuiltIns: 'usage',
-        corejs: '3.1.4',
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: {
+        edge: '15',
+        firefox: '60',
+        chrome: '64',
+        safari: '11.1',
+        esmodules: true,
       },
-    ],
+      useBuiltIns: 'usage',
+      corejs: '3.0.0',
+    },
   ],
-};
+];
+
+module.exports = { presets };
