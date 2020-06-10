e797de8757ff53b8126e1428acfd9ee58b274b66
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-06-10 20:49:01 +0200

    Fixed bugs

--- src/js/constants/config.js
+++ src/js/constants/config.js
@@ -1,7 +1,7 @@
 const config = {
   SERVER_URL: 'https://api.mydiploma.website',
-  NEWSAPI_URL: 'https://newsapi.org/v2/everything',
-  NEWSAPI_TOKEN: '436d782cf30e4996a880aaac9b60e14c',
+  NEWSAPI_URL: 'https://praktikum.tk/news/v2/everything',
+  NEWSAPI_TOKEN: '2c834e0311134ff0873a72dcfc722e17',
   ESCAPE_CODE: 27,
   ONE_DAY: 24 * 60 * 60 * 1000,
   LAST_DAY: 7,
